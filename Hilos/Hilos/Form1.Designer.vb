5ae275aa18fc31c560552a3c1463a7284a092ce5
Martin Servidio <86500202+MartinServidio@users.noreply.github.com>
2022-08-16 12:18:09 -0300

    Add files via upload

---
+++ Hilos/Hilos/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(47, 40)
+        Me.ProgressBar1.Margin = New System.Windows.Forms.Padding(2)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(332, 18)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Location = New System.Drawing.Point(47, 142)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(332, 83)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button2.ForeColor = System.Drawing.Color.Blue
+        Me.Button2.Location = New System.Drawing.Point(47, 81)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(90, 38)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Iniciar"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button1.ForeColor = System.Drawing.Color.Black
+        Me.Button1.Location = New System.Drawing.Point(298, 81)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(81, 38)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Cancelar"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.Location = New System.Drawing.Point(396, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Label1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(168, 91)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Label1.Location = New System.Drawing.Point(149, 73)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(142, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Escriba la cantidad de lineas"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(447, 256)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Hilos/Hilos/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Threading
+Imports System.IO.StreamWriter
+Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+    <STAThread()>
+    Public Shared Sub Main()
+        Dim hilo1 As New Thread(AddressOf Primero)
+        Dim hilo2 As New Thread(AddressOf Segundo)
+        hilo1.Start()
+        hilo2.Start()
+    End Sub
+
+    Public Shared Sub Primero()
+        Dim Texto As String = Form1.TextBox2.Text
+        Segundo(Texto)
+    End Sub
+    Public Shared Sub Segundo(Aux As String)
+        Dim Max As Integer = Form1.TextBox1.Text
+        Form1.ProgressBar1.Maximum = Max
+        Dim Palabras As String() = Form1.TextBox2.Text.Split(" ")
+        Dim Palabra As String = ""
+        Dim Contador As Integer = 0
+        Dim Linea As String = ""
+        Contador = 0
+
+        Dim path As String = "C:\Users\PC1\Desktop\Hilos.txt"
+        Dim fs As FileStream = File.Create(path)
+        Dim info As Byte() = New UTF8Encoding(True).GetBytes(Aux)
+        fs.Write(info, 0, info.Length)
+
+        For Each Palabra In Palabras
+            Linea = Linea & Palabra & " "
+            Contador += 1
+        Next
+
+        Dim lineas As String() = Form1.TextBox2.Lines()
+        Contador -= 1
+        For Each lin As String In lineas
+            Dim values As Object() = {lin}
+            Contador += 1
+        Next
+
+        Form1.ProgressBar1.Value = Contador
+        Form1.Label2.Text = (Contador / Max) * 100 & "%"
+        Form1.Label2.Refresh()
+        fs.Close()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        Primero()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Val(TextBox1.Text) > 0 Then
+            Button2.Visible = False
+            TextBox1.Enabled = False
+        Else
+            MsgBox("Inserte cantidad maxima de lineas de texto")
+        End If
+    End Sub
+End Class
